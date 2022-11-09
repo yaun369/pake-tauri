0a5770d9a9631b79988292630b959a50e6940669
ymm <geekreading@163.com>
2022-11-09 15:18:41 +0800

    fix: 去掉广告

--- src-tauri/src/pake.js
+++ src-tauri/src/pake.js
@@ -1,4 +1,4 @@
-// @ts-check
+// @ts-nocheck
 
 /**
  * @typedef {string} KeyboardKey `event.key` 的代号，
@@ -17,117 +17,57 @@
  * @type {Record<KeyboardKey, OnKeyDown>}
  */
 const metaKeyShortcuts = {
-  'ArrowUp': () => scrollTo(0, 0),
-  'ArrowDown': () => scrollTo(0, document.body.scrollHeight),
-  '[': () => window.history.back(),
-  ']': () => window.history.forward(),
-  'r': () => window.location.reload(),
-  '-': () => zoomOut(),
-  '=': () => zoomIn(),
-  '0': () => zoomCommon(() => '100%'),
-}
+  ArrowUp: () => scrollTo(0, 0),
+  ArrowDown: () => scrollTo(0, document.body.scrollHeight),
+  "[": () => window.history.back(),
+  "]": () => window.history.forward(),
+  r: () => window.location.reload(),
+  "-": () => zoomOut(),
+  "=": () => zoomIn(),
+  0: () => zoomCommon(() => "100%"),
+};
 
-window.addEventListener('DOMContentLoaded', (_event) => {
-  const style = document.createElement('style');
+window.addEventListener("DOMContentLoaded", (_event) => {
+  const style = document.createElement("style");
   style.innerHTML = `
-    // mini twitter 代码存到 dist 下面，为了防止干扰，需要的时候 copy 过来即可
-
-    .panel.give_me .nav_view {
-      top: 154px !important;
+    #iaujwnefhw {
+      display: none;
+    }
+    
+    #fkasjgf {
+      display: none;
     }
 
-    .columns .column #header{
-      padding-top: 30px;
+    #sajdhfbjwhe {
+      display: none;
     }
 
-    #page .main_header, #ReactApp .lark .main-wrapper > div, #ReactApp .lark .sidebar-user-info, .explore-wrapper .yuque-header-wrapper {
-      padding-top: 20px;
-    }
-
-    #page #footer-wrapper,
-    .drawing-board .toolbar .toolbar-action,
-    .c-swiper-container,
-    .download_entry,
-    .lang, .copyright,
-    .wwads-cn, .adsbygoogle,
-    #Bottom > div.content > div.inner,
-    #Rightbar .sep20:nth-of-type(5),
-    #Rightbar > div.box:nth-child(4),
-    #Main > div.box:nth-child(8) > div
-    #Wrapper > div.sep20,
-    #Main > div.box:nth-child(8),
-    #masthead-ad,
-    #Rightbar > div:nth-child(6) > div.sidebar_compliance {
-      display: none !important;
-    }
-
-    ytd-masthead>#container.style-scope.ytd-masthead {
-      padding-top: 12px !important;
-    }
-
-    .wrap.h1body-exist.max-container > div.menu-tocs > div.menu-btn{
-      top: 28px;
-    }
-
-    #Wrapper{
-      background-color: #F8F8F8 !important;
-      background-image:none !important;
-    }
-
-    #Top {
-      border-bottom: none;
-    }
-
-    .container-with-note #home, .container-with-note #switcher{
-      top: 30px;
-    }
-
-    .geist-page nav.dashboard_nav__PRmJv {
-      padding-top:10px;
-    }
-
-    .geist-page .submenu button{
-      margin-top:24px;
-    }
-
-    #pack-top-dom:active {
-      cursor: grabbing;
-      cursor: -webkit-grabbing;
-    }
-
-    #pack-top-dom{
-      position:fixed;
-      background:transparent;
-      top:0;
-      width: 100%;
-      height: 20px;
-      cursor: grab;
-      cursor: -webkit-grab;
-      z-index: 90000;
+    .hthb-row {
+      display: none;
     }
   `;
   document.head.append(style);
-  const topDom = document.createElement('div');
-  topDom.id = 'pack-top-dom';
+  const topDom = document.createElement("div");
+  topDom.id = "pack-top-dom";
   document.body.appendChild(topDom);
 
-  const domEl = document.getElementById('pack-top-dom');
+  const domEl = document.getElementById("pack-top-dom");
 
-  domEl.addEventListener('mousedown', (e) => {
+  domEl.addEventListener("mousedown", (e) => {
     if (e.buttons === 1 && e.detail !== 2) {
-      window.ipc.postMessage('drag_window');
+      window.ipc.postMessage("drag_window");
     }
   });
 
-  domEl.addEventListener('touchstart', () => {
-    window.ipc.postMessage('drag_window');
+  domEl.addEventListener("touchstart", () => {
+    window.ipc.postMessage("drag_window");
   });
 
-  domEl.addEventListener('dblclick', () => {
-    window.ipc.postMessage('fullscreen');
+  domEl.addEventListener("dblclick", () => {
+    window.ipc.postMessage("fullscreen");
   });
 
-  document.addEventListener('keyup', function (event) {
+  document.addEventListener("keyup", function (event) {
     const preventDefault = (f) => {
       event.preventDefault();
       f();
@@ -138,11 +78,11 @@ window.addEventListener('DOMContentLoaded', (_event) => {
     }
   });
 
-  document.addEventListener('click', (e) => {
-    const origin = e.target.closest('a');
+  document.addEventListener("click", (e) => {
+    const origin = e.target.closest("a");
     const href = origin.href;
     if (href) {
-      origin.target = '_self';
+      origin.target = "_self";
     }
   });
 });
@@ -150,9 +90,9 @@ window.addEventListener('DOMContentLoaded', (_event) => {
 setDefaultZoom();
 
 function setDefaultZoom() {
-  const htmlZoom = window.localStorage.getItem('htmlZoom');
+  const htmlZoom = window.localStorage.getItem("htmlZoom");
   if (htmlZoom) {
-    document.getElementsByTagName('html')[0].style.zoom = htmlZoom;
+    document.getElementsByTagName("html")[0].style.zoom = htmlZoom;
   }
 }
 
@@ -160,21 +100,17 @@ function setDefaultZoom() {
  * @param {(htmlZoom: string) => string} [zoomRule]
  */
 function zoomCommon(zoomRule) {
-  const htmlZoom = window.localStorage.getItem('htmlZoom') || '100%';
-  const html = document.getElementsByTagName('html')[0];
+  const htmlZoom = window.localStorage.getItem("htmlZoom") || "100%";
+  const html = document.getElementsByTagName("html")[0];
   const zoom = zoomRule(htmlZoom);
   html.style.zoom = zoom;
-  window.localStorage.setItem('htmlZoom', zoom);
+  window.localStorage.setItem("htmlZoom", zoom);
 }
 
 function zoomIn() {
-  zoomCommon((htmlZoom) =>
-    `${Math.min(parseInt(htmlZoom) + 10, 200)}%`
-  );
+  zoomCommon((htmlZoom) => `${Math.min(parseInt(htmlZoom) + 10, 200)}%`);
 }
 
 function zoomOut() {
-  zoomCommon((htmlZoom) =>
-    `${Math.max(parseInt(htmlZoom) - 10, 30)}%`
-  );
+  zoomCommon((htmlZoom) => `${Math.max(parseInt(htmlZoom) - 10, 30)}%`);
 }
